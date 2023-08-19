960d0f5cf4c5b568db1e3452d0f1e9dd82f96964
elenalev2686 <elenalev2686@gmail.com>
2023-08-19 17:31:46 +0700

    test setup jococo

--- src/test/java/ru/netology/githaba/service/StatisticsServiceTest.java
+++ src/test/java/ru/netology/githaba/service/StatisticsServiceTest.java
@@ -1,6 +1,8 @@
 package ru.netology.githaba.service;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+
 public class StatisticsServiceTest {
     @Test
     void findMax() {
@@ -13,12 +15,13 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     void findMaxFromMiddle() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 30, 8, 6, 11, 11, 12};
-        long expected =30;
+        long expected = 30;
 
         long actual = service.findMax(incomesInBillions);
 
